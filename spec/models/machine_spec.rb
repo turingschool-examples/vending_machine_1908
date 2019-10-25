10a1ed1066162ea76eb3f4a58c05cc1f6d8fda4b
John Travers <johnk.travers@gmail.com>
2019-10-25 10:32:29 -0600

    Add test for snack_count instance method

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Machine, type: :model do
 
     it { should have_many(:snacks).through(:machine_snacks) }
   end
+
+  describe 'instance methods' do
+    it 'can count the number of snacks it has' do
+      owner  = Owner.create(name: "Sam's Snacks")
+      wilmas = owner.machines.create(location: "Wilma's Diner")
+
+      wilmas.snacks.create(name: 'Pop Rocks', price: 1.50)
+      wilmas.snacks.create(name: 'White Castle Burger', price: 3.50)
+      wilmas.snacks.create(name: 'Flaming Hot Cheetos', price: 2.00)
+
+      expect(wilmas.snack_count).to eq(3)
+    end
+  end
 end
