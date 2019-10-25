044c29f8e1f96de104e6f769a397a3034c3cfa13
Jon Patterson <JonPatt92@gmail.com>
2019-10-25 10:05:16 -0600

    Add avg_snack_price method

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,8 @@ class Machine < ApplicationRecord
   has_many :snacks, through: :machine_snacks
 
   validates_presence_of :location
+
+  def avg_snack_price
+    snacks.average(:price).to_f.round(2)
+  end
 end
