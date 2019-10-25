4b4f2ef3cd79b21cca49aff55913433825ea3931
Laura Schulz <lrs8810@gmail.com>
2019-10-25 10:38:00 -0600

    Add snack model spec with validations

---
+++ spec/models/snack_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Snack, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :price }
+    it { should have_many :machine_snacks }
+    it { should have_many :machines }
+  end
+end
