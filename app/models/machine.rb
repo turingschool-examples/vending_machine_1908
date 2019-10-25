24fdf10a2cea0619f7e39f511288438a7bf9be26
David Gomez <davdg25@gmail.com>
2019-10-25 10:45:50 -0600

    added model method for averge price

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  def snack_average_price
+    snacks.average(:price)
+  end
 end
