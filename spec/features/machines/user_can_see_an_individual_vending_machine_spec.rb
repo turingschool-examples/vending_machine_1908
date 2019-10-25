bddf4e7d91100e72522778ff88a0cc27d5868e4c
Graham Thompson <grwthomps@gmail.com>
2019-10-25 09:54:30 -0600

    Create feature test for showing all snacks with prices on machine show page

--- spec/features/machines/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/machines/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  it 'can see all snacks with prices' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    nuts = dons.snacks.create!(name: 'Organic Mixed Nuts', price: 10)
+    chips = dons.snacks.create!(name: 'Biff\'s Chips', price: 8)
+
+    visit machine_path(dons)
+
+    within "#snacks-#{nuts.id}" do
+      expect(page).to have_content(nuts.name)
+      expect(page).to have_content(nuts.price)
+    end
+
+    within "#snacks-#{chips.id}" do
+      expect(page).to have_content(chips.name)
+      expect(page).to have_content(chips.price)
+    end
+  end
 end
