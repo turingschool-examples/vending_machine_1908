2ad42e0996a958c9bedd32d9efdb10c6292202b4
Brian Bower <brian@brianbower.com>
2019-10-25 09:59:39 -0600

    Refactor to use seperate relationship and validtions it blocks

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Owner, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
     it { should have_many :machines }
   end
 end
