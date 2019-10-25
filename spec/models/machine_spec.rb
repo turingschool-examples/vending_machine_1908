b8daa130159bfd5f334dddc45919081ab62181a7
Graham Thompson <grwthomps@gmail.com>
2019-10-25 09:35:06 -0600

    Update machine model test to check for many snacks

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+    it { should have_many :snacks }
   end
 end
