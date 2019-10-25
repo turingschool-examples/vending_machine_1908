6eea3e4371e73e9315e18269f078c0c36c9b3d4a
Rachel Lew <rlew07@gmail.com>
2019-10-25 09:33:56 -0600

    Sets up many to many relationship between machines and snacks

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,8 @@
 class Machine < ApplicationRecord
   validates_presence_of :location
-  
+
   belongs_to :owner
+
+  has_many :machine_snacks
+  has_many :snacks, through: :machine_snacks
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+    it { should have_many :snacks }
+    it {should have_many(:snacks).through(:machine_snacks)}
   end
 end
