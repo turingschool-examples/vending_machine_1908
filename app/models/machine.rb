a0c98eae7286b5587e51a2129bca396754b61658
Laura Schulz <lrs8810@gmail.com>
2019-10-25 10:39:54 -0600

    Add machine, snack, and machine_snack models with relationships, validations, and instance methods

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,15 @@
 class Machine < ApplicationRecord
-  validates_presence_of :location
-  
   belongs_to :owner
+  has_many :machine_snacks
+  has_many :snacks, through: :machine_snacks
+
+  validates_presence_of :location
+
+  def average_snack_price
+    snacks.average(:price)
+  end
+
+  def snack_count
+    snacks.count(:id)
+  end
 end
---
+++ app/models/snack.rb
@@ -0,0 +1,7 @@
+class Snack < ApplicationRecord
+  has_many :machine_snacks
+  has_many :machines, through: :machine_snacks
+
+  validates_presence_of :name
+  validates_presence_of :price
+end
