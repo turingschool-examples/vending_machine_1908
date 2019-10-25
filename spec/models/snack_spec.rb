2be5ece19d73ee2276dc6711efaeefdbd0fd0ceb
John Travers <johnk.travers@gmail.com>
2019-10-25 09:14:28 -0600

    Add snack validation tests

---
+++ spec/models/snack_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Snack, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :price }
+
+    it { should validate_numericality_of :price }
+  end
+
+
+end
