7a50b14ff29f05cc0507604a36191cd5f8485910
Ryan Hantak <rhantak@gmail.com>
2019-10-25 09:55:18 -0600

    Add method for machines to calculate the average price of their items

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -5,4 +5,8 @@ class Machine < ApplicationRecord
   has_many :snacks, through: :machine_snacks
 
   belongs_to :owner
+
+  def average_price
+    snacks.average(:price)
+  end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many :snacks }
   end
+
+  describe 'instance methods' do
+    it "can calculate the average price of its selection" do
+      owner_1 = Owner.create(name: "Bob G")
+      machine_1 = owner_1.machines.create(location: "Turing Basement")
+      snack_1 = Snack.create(name: "Hot Cheetos", price: 1.50)
+      snack_2 = Snack.create(name: "Takis", price: 1.20)
+      MachineSnack.create(machine_id: machine_1.id, snack_id: snack_1.id)
+      MachineSnack.create(machine_id: machine_1.id, snack_id: snack_2.id)
+
+      expect(machine_1.average_price).to eq(1.35)
+    end
+  end
 end
