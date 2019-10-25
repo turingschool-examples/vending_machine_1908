96b58b0e37959fd5daff9c681d66cdc5aa247f8b
John Travers <johnk.travers@gmail.com>
2019-10-25 10:34:18 -0600

    Add route to snack show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: "owners#index"
 
   resources :owners do
-    resources :machines, only: [:index]
+    resources :machines, only: :index
   end
 
-  resources :machines, only: [:show]
+  resources :machines, only: :show
+  resources :snacks,   only: :show
 end
