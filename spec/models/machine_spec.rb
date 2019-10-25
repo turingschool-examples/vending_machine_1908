334fdad90ec3f3c2f024eaccfe670257ec39ee47
Brian Bower <brian@brianbower.com>
2019-10-25 10:00:12 -0600

    Add snacks relationship validation

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
+  end
+
+  describe 'relationships' do
     it { should belong_to :owner }
+    it { should have_many :snacks }
   end
 end
