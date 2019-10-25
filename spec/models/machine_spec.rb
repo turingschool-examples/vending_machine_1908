9f49ff6859f25f17096e32915db8d843e984f280
John Travers <johnk.travers@gmail.com>
2019-10-25 09:21:05 -0600

    Add has_many snacks relationship test

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
+  end
+
+  describe 'relationships' do
     it { should belong_to :owner }
+
+    it { should have_many(:snacks).through(:machine_snacks) }
   end
 end
