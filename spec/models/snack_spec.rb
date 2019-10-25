e1b3d9b121870c72b00360f02cf4bb88f1bb00ce
Graham Thompson <grwthomps@gmail.com>
2019-10-25 10:20:56 -0600

    Refactor model tests to many-to-many relationship

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Snack, type: :model do
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :price}
-    it {should belong_to :machine}
+    it { should have_many :machine_snacks}
+    it { should have_many(:machines).through(:machine_snacks) }
   end
 
   describe 'class methods' do
