18b8502b4afc03becc66d8d08c25db93e697db58
John Travers <johnk.travers@gmail.com>
2019-10-25 09:53:26 -0600

    Add test for average_price class method

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -13,5 +13,18 @@ RSpec.describe Snack, type: :model do
     it { should have_many(:machines).through(:machine_snacks) }
   end
 
+  describe 'class methods' do
+    it 'can calculate the average price' do
+      owner  = Owner.create(name: "Sam's Snacks")
+      dons   = owner.machines.create(location: "Don's Mixed Drinks")
+
+      dons.snacks.create(name: 'White Castle Burger', price: 3.50)
+      dons.snacks.create(name: 'Pop Rocks', price: 1.50)
+      dons.snacks.create(name: 'Flaming Hot Cheetos', price: 2.00)
+      dons.snacks.create(name: 'Snickers Bar', price: 1.50)
+
+      expect(Snack.average_price).to eq(2.125)
+    end
+  end
 
 end
