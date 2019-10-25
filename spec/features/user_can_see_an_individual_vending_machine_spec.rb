42e3442dc459dd65a326ccf13febd2232069432a
Jon Patterson <JonPatt92@gmail.com>
2019-10-25 10:05:04 -0600

    Tweak test to be more robust, test avg_snack_price machine model method

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
   scenario 'they see all the snacks from that machine, their price, and the average snack price.' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    bubbas  = owner.machines.create(location: "Bubbas Shrimp")
+
+    shrimp = Snack.create(name: "Shrimp", price: 4)
     cheetos = Snack.create(name: "Cheetos", price: 4)
     chips = Snack.create(name: "BBQ Potato Chips", price: 5)
     granola = Snack.create(name: "Healthy Wood Chips", price: 15)
+
+    bubbas.machine_snacks.create(machine_id: bubbas.id, snack_id: shrimp.id)
     dons.machine_snacks.create(machine_id: dons.id, snack_id: cheetos.id)
     dons.machine_snacks.create(machine_id: dons.id, snack_id: chips.id)
     dons.machine_snacks.create(machine_id: dons.id, snack_id: granola.id)
@@ -17,6 +22,7 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     expect(page).to have_content("Cheetos: $3")
     expect(page).to have_content("BBQ Potato Chips: $5")
     expect(page).to have_content("Healthy Wood Chips: $15")
-    expect(page).to have_content("Average Price: $8")
+    expect(page).to have_content("Average Price: $8.0")
+
   end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it {should have_many(:snacks).through(:machine_snacks)}
   end
+
+  describe 'instance methods' do
+    it 'avg_snack_price' do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      bubbas  = owner.machines.create(location: "Bubbas Shrimp")
+
+      shrimp = Snack.create(name: "Shrimp", price: 4)
+      cheetos = Snack.create(name: "Cheetos", price: 4)
+      chips = Snack.create(name: "BBQ Potato Chips", price: 5)
+      granola = Snack.create(name: "Healthy Wood Chips", price: 15)
+
+      bubbas.machine_snacks.create(machine_id: bubbas.id, snack_id: shrimp.id)
+      dons.machine_snacks.create(machine_id: dons.id, snack_id: cheetos.id)
+      dons.machine_snacks.create(machine_id: dons.id, snack_id: chips.id)
+      dons.machine_snacks.create(machine_id: dons.id, snack_id: granola.id)
+
+      expect(dons.avg_snack_price).to eq(8.0)
+    end
+  end
 end
