fd9cd722890a7055b0d91d074816aaa831c1c56a
Laura Schulz <lrs8810@gmail.com>
2019-10-25 10:45:09 -0600

    Refactor models to be consistent

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,9 +1,10 @@
 class Machine < ApplicationRecord
+  validates_presence_of :location
+
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
 
-  validates_presence_of :location
 
   def average_snack_price
     snacks.average(:price)
